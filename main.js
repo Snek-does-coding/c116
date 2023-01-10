06c64be2303c3808d86fe61d98d3a90685e120a8
Snek-does-coding <108309794+Snek-does-coding@users.noreply.github.com>
2023-01-09 19:12:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ function classify()
+ {
+    navigator.mediaDevices.getUserMedia({ auto: true });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/uTR7XvOXp/model.json', modelReady);
+ }
+ 
